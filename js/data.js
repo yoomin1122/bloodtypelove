e0713a142cac0c8a5c71af1a34fc4b844743ad92
Yoo Ho <79196965+yoomin1122@users.noreply.github.com>
2022-06-07 23:15:26 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,47 +1,38 @@
-/*
-    {name:'A', value:0, key:0},
-    {name:'B', value:0, key:1},
-    {name:'AB', value:0, key:2}
-    {name:'O', value:0, key:3},
-*/
-
-
-
 const qnaList = [
     {
         q: '1. 연애할때 어떤 생각이 드나요?',
         a: [
-            {answer : '1. 사랑스러워', type:[0]},
-            {answer : '2. 핫이슈', type:[1]},
-            {answer : '3. 오로나민C', type:[2]},
-            {answer : '3. 오로나민C', type:[3]}
+            {answer : '1. 허걱', type:[0]},
+            {answer : '2. 허헉', type:[1]},
+            {answer : '3. 허걱ㄱ', type:[2]},
+            {answer : '4. 허허ㅓ헣ㄱ', type:[3]}
         ]
     },
     {
         q: '2. 식상한 말이',
         a: [
-            {answer : '1. 사랑스러워', type:[0]},
-            {answer : '2. 핫이슈', type:[1]},
-            {answer : '3. 오로나민C', type:[2]},
-            {answer : '3. 오로나민C', type:[3]}
+            {answer : '1. 허걱', type:[0]},
+            {answer : '2. 허헉', type:[1]},
+            {answer : '3. 허걱ㄱ', type:[2]},
+            {answer : '4. 허허ㅓ헣ㄱ', type:[3]}
         ]
     },
     {
         q: '3. 내 가사가 될 줄 몰랐어',
         a: [
-            {answer : '1. 사랑스러워', type:[0]},
-            {answer : '2. 핫이슈', type:[1]},
-            {answer : '3. 오로나민C', type:[2]},
-            {answer : '3. 오로나민C', type:[3]}
+            {answer : '1. 허걱', type:[0]},
+            {answer : '2. 허헉', type:[1]},
+            {answer : '3. 허걱ㄱ', type:[2]},
+            {answer : '4. 허허ㅓ헣ㄱ', type:[3]}
         ]
     },
     {
         q: '4. 얼마 안됐네',
         a: [
-            {answer : '1. 사랑스러워', type:[0]},
-            {answer : '2. 핫이슈', type:[1]},
-            {answer : '3. 오로나민C', type:[2]},
-            {answer : '3. 오로나민C', type:[3]}
+            {answer : '1. 허걱', type:[0]},
+            {answer : '2. 허헉', type:[1]},
+            {answer : '3. 허걱ㄱ', type:[2]},
+            {answer : '4. 허허ㅓ헣ㄱ', type:[3]}
         ]
     },
 ]
