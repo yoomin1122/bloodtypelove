bb91a8fce73d81ca931e43c74e06cfff592375f3
Yoo Ho <79196965+yoomin1122@users.noreply.github.com>
2022-06-07 23:10:59 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,66 @@
+/*
+    {name:'A', value:0, key:0},
+    {name:'B', value:0, key:1},
+    {name:'AB', value:0, key:2}
+    {name:'O', value:0, key:3},
+*/
+
+
+
+const qnaList = [
+    {
+        q: '1. 연애할때 어떤 생각이 드나요?',
+        a: [
+            {answer : '1. 사랑스러워', type:[0]},
+            {answer : '2. 핫이슈', type:[1]},
+            {answer : '3. 오로나민C', type:[2]},
+            {answer : '3. 오로나민C', type:[3]}
+        ]
+    },
+    {
+        q: '2. 식상한 말이',
+        a: [
+            {answer : '1. 사랑스러워', type:[0]},
+            {answer : '2. 핫이슈', type:[1]},
+            {answer : '3. 오로나민C', type:[2]},
+            {answer : '3. 오로나민C', type:[3]}
+        ]
+    },
+    {
+        q: '3. 내 가사가 될 줄 몰랐어',
+        a: [
+            {answer : '1. 사랑스러워', type:[0]},
+            {answer : '2. 핫이슈', type:[1]},
+            {answer : '3. 오로나민C', type:[2]},
+            {answer : '3. 오로나민C', type:[3]}
+        ]
+    },
+    {
+        q: '4. 얼마 안됐네',
+        a: [
+            {answer : '1. 사랑스러워', type:[0]},
+            {answer : '2. 핫이슈', type:[1]},
+            {answer : '3. 오로나민C', type:[2]},
+            {answer : '3. 오로나민C', type:[3]}
+        ]
+    },
+]
+
+const infoList = [
+    {
+        name: 'A형',
+        desc: '와 ㄷㄷ'
+    },
+    {
+        name: 'B형',
+        desc: '와 ㄷㄷㄷ'
+    },
+    {
+        name: 'AB형',
+        desc: '와 ㄷㄷㄷㄷ'
+    },
+    {
+        name: 'O형',
+        desc: '와 ㄷㄷㄷㄷㄷ'
+    },
+]
